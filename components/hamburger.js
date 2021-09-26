8b15c3143c2891663ea342aa9f36b34b97a47daa
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-26 11:28:25 -0400

    menu

--- components/hamburger.js
+++ components/hamburger.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default function hamburger() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function Hamburger() {
+  return (
+    <div className="flex flex-col space-y-5 h-screen absolute">
+      <h3>home</h3>
+      <h3>about us</h3>
+      <h3>our team</h3>
+      <h3>mailing list</h3>
+      <h3>contact</h3>
+    </div>
+  );
 }
--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Hamburger from "./hamburger";
 
 export default function Navbar() {
+  const [sideBar, setsideBar] = useState(false);
+
   return (
-    <div className="flex align-middle py-6 justify-center">
-      <h1>Bayview Model UN</h1>
+    <div className="grid grid-cols-3 px-4 py-6">
+      <svg viewBox="0 0 100 80" width="40" height="40">
+        <rect width="80" height="10"></rect>
+        <rect y="20" width="80" height="10"></rect>
+        <rect y="40" width="80" height="10"></rect>
+      </svg>
+      <div className="justify-self-center">
+        <h1>Bayview Model UN</h1>
+      </div>
+      <div className="col-start-1">
+        <Hamburger></Hamburger>
+      </div>
     </div>
   );
 }
