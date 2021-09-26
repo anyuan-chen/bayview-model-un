a861fe2ab49ff6491c5613b98642fc511283d88d
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-25 21:32:31 -0400

    init

---
+++ components/navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="flex align-middle py-6 justify-center">
+      <h1>Bayview Model UN</h1>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ["Lora"],
+        body: ["Nunito"],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
