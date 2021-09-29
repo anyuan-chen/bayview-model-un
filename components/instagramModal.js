56c1a73de15afa49029cbbd522da84eb49f0b9b1
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-29 17:54:42 -0400

    instagram modals

--- components/instagramModal.js
+++ components/instagramModal.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 
 export default function InstagramModal(props) {
   return (
-    <div className="fixed w-full h-full z-50 top-0 left-0 flex items-center justify-center">
-      <div className="flex w-7/12 h-5/6">
+    <div className="fixed w-full h-full z-30 top-0 left-0 flex items-center justify-center bg-opacity-50 bg-black bg-blend-normal	">
+      <div className="flex w-9/12 h-5/6 z-50">
         <img src={props.src}></img>
-        <div className="bg-white">
-          <h1>{props.username}</h1>
-          <p>{props.caption}</p>
+        <div className="bg-white z-50">
+          <div className="mx-8 flex flex-col items-start pt-5">
+            <h1 className="font-bold text-base">{props.username}</h1>
+            <p className="font-body text-sm text-left pt-2">{props.caption}</p>
+          </div>
         </div>
       </div>
     </div>
