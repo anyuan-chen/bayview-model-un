4900fb198992ea6775d12181b5d2aab0337dd87a
magicalsoup <amagicalsoup@gmail.com>
2021-09-30 14:01:05 -0400

    Adding css to sliders

--- components/ImageSlider.js
+++ components/ImageSlider.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import  sliderData  from "../data/sliderData.js";
 export default function ImageSlider ({slides}) {
 
     const length = sliderData.length;
 
     const [current, setCurrent] = useState(0);
-    const [previous, setPrevious] = useState(length-1);
-    const [next, setNext] = useState(1);
+
+    const [prevFadeIn, setPrevFadeIn] = useState(false);    
+    const [nextFadeIn, setNextFadeIn] = useState(false);
+
+
     
     /*if(!Array.isArray(slides) || slides.length <= 0) {
         console.log(Array.isArray(slides));
@@ -14,42 +17,58 @@ export default function ImageSlider ({slides}) {
         return null;
         
     }*/
-
     
 
     const nextSlide = () => {
+        setNextFadeIn(true);
+        setPrevFadeIn(false);
         setCurrent(current === length - 1? 0 : current + 1);
-        setPrevious(previous == length - 1? 0 : previous + 1);
-        setNext(next === length - 1? 0 : next + 1);
+    
     };
 
     const prevSlide = () => {
+        setNextFadeIn(false);
+        setPrevFadeIn(true);
         setCurrent(current === 0? length - 1 : current - 1);
-        setPrevious(previous === 0? length - 1 : previous - 1);
-        setNext(next === 0? length - 1 : next - 1);
     }
 
     
+    const getNext = () => {
+        return (current + 1) % length;
+    }
+
+    const getPrevious = () => {
+        return (current - 1 + length) % length;
+    }
+
+    const setCurrentImageClassName = () => {
+        if(prevFadeIn === false && nextFadeIn === false) {
+            return "";
+        }
+        if(prevFadeIn) return "prevFadeIn";
+        return "nextFadeIn";
+    }
+
     return (
         <div className="flex items-center h-72 space-evenly justify-evenly flex-col">
             <div className="flex justify-content center w-4/5">
-                <section className="flex justify-around gap-3">
-                    <div className="slide-previous">
-                        <img src={sliderData[previous].image} className="h-72 w-screen object-scale-down"/>
+                <section className="flex justify-around gap-3"> 
+                    <div key={Math.random()} className={prevFadeIn? "prevFadeIn" : "nextFadeIn"}>  
+                        <img src={sliderData[getPrevious()].image} className="h-72 w-screen object-scale-down"/>
                     </div>
-                    <div className="slide-current">
-                        <img src={sliderData[current].image} className="h-80 w-screen object-scale-down"/>        
+                    <div key={Math.random()} className={setCurrentImageClassName()}>
+                        <img src={sliderData[current].image} className="h-72 w-screen object-scale-down"/>        
                     </div>
-                    <div className="slide-next">
-                        <img src={sliderData[next].image} className ="h-72 w-screen object-scale-down"/>
-                    
+                    <div key={Math.random()} className={nextFadeIn? "nextFadeIn" : "prevFadeIn"}>
+                        <img src={sliderData[getNext()].image} className ="h-72 w-screen object-scale-down"/>
                     </div>
                 </section>
                 </div>
-            <div className="flex item-end">
-                <button className="px-16 py-2 bg-white" onClick={prevSlide}>left arrow</button>
-                <button className="px-16 py-2 bg-white" onClick={nextSlide}>right arrow</button>
+            <div className="flex item-end gap-3">
+                <button className="px-16 py-2 bg-gray-400" onClick={prevSlide}>left arrow</button>
+                <button className="px-16 py-2 bg-gray-400" onClick={nextSlide}>right arrow</button>
             </div>
         </div>
     );
+
 }
