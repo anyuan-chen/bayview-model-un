2da1c95d664c96991cebca81d1c44ed1395302ba
magicalsoup <amagicalsoup@gmail.com>
2021-10-08 21:12:50 -0400

    fixing modals

--- components/post.js
+++ components/post.js
@@ -15,7 +15,7 @@ export default function Post(props) {
   return (
     <div className="shadow-xl flex justify-center">
       <button onClick={toggleModal}>
-        <img src={props.src} className="h-full w-full object-scale-down"></img>
+        <img src={props.src} className="h-full w-screen object-scale-down"></img>
         <div className={getClassName()}>
           <InstagramModal
             src={props.src}
