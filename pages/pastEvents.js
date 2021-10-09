2172ab2203581847dbad6208720a665f00f1c6d2
magicalsoup <amagicalsoup@gmail.com>
2021-10-09 14:30:07 -0400

    nit fixes on pastevents

--- pages/pastEvents.js
+++ pages/pastEvents.js
@@ -18,7 +18,7 @@ export default function PastEvents() {
     <div>
       <Navbar></Navbar>
       <div className="text-white bg-main flex pl-6 py-12 mb-6">
-        <h1 className="bold text-4xl pb-6 pl-12">Past Events</h1>
+        <h1 className="bold text-4xl pb-6 pl-1 sm:pl-12">Past Events</h1>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-8 px-6 mb-10">{events}</div>
     </div>
