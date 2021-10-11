aba808442894b748d60c168268c66d1af7c73545
magicalsoup <amagicalsoup@gmail.com>
2021-10-11 13:24:33 -0400

    fix team

--- pages/team.js
+++ pages/team.js
@@ -18,7 +18,7 @@ export default function Team() {
       <div className="h-40 bg-main flex items-center justify-center">
         <h1 className="text-white font-body text-3xl"> Our Team</h1>
       </div>
-      <div className="flex justify-center my-8 px-8 xl:px-0">
+      <div className="flex justify-center my-8 lg:px-8 xl:px-0">
         <div className="max-w-5xl mt-8 grid md:grid-cols-2 sm:grid-cols-1 gap-10">
           {teamMembers}
         </div>
